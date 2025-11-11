e5ec0245103cc0eb27743ebe8943ab1180ac7c0d
didntpot <didntpot@protonmail.com>
2025-11-11 15:57:23 +0000

    Hi (kill me)

--- compile.sh
+++ compile.sh
@@ -1292,7 +1292,8 @@ $CONFIGURE_FLAGS >> "$DIR/install.log" 2>&1
 # before the system/PHP OpenSSL headers, otherwise we get
 # OPENSSL_PUT_ERROR / ASN1 / etc build failures.
 if [ -f "ext/grpc/Makefile" ] && [ -d "ext/grpc/third_party/boringssl-with-bazel/src/include" ]; then
-  sed -i 's|^AM_CPPFLAGS = |AM_CPPFLAGS = -I$(srcdir)/third_party/boringssl-with-bazel/src/include |' ext/grpc/Makefile
+  sed -i 's|^AM_CPPFLAGS =|AM_CPPFLAGS = -I$(srcdir)/third_party/boringssl-with-bazel/src/include |' ext/grpc/Makefile
+  sed -i "s|-I$INSTALL_DIR/include||g" ext/grpc/Makefile
 fi
 
 write_compile
